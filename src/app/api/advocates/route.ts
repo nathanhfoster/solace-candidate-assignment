703d50112ba048e5421ac72f9bdb1139fccd1111
Nathan <nateinthegame@gmail.com>
2025-01-16 15:27:23 -0500

    Connected GET API to database with the seeded advocates table

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,12 +1,8 @@
 import db from "../../../db";
-import { advocates } from "../../../db/schema";
-import { advocateData } from "../../../db/seed/advocates";
+import { advocates } from "@/db/schemas/advocates";
 
-export async function GET() {
-  // Uncomment this line to use a database
-  // const data = await db.select().from(advocates);
-
-  const data = advocateData;
+export async function GET(): Promise<Response> {
+  const data = await db.select().from(advocates);
 
   return Response.json({ data });
 }
